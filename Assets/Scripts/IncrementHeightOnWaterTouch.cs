8887124eccadc82ecb7414cf2bc254c9937fe4af
Leo <leo@leo-sa.com>
2016-12-10 18:46:20 -0600

    Added water particle system

---
+++ Assets/Scripts/IncrementHeightOnWaterTouch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class IncrementHeightOnWaterTouch : MonoBehaviour {
+
+    public string[] collisionTags;
+    public float sizeIncrement;
+
+	// Use this for initialization
+	void Start () {
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+            gameObject.transform.Translate(new Vector3(0, sizeIncrement, 0));
+    }
+}
