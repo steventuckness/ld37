d8c0848a291a6e74aac85f4bc7f21c88cf7658fa
lsa <leo@leo-sa.com>
2016-12-10 21:32:13 -0600

    Added realistic water

---
+++ Assets/Scripts/IncrementHeight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncrementHeight : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Translate (new Vector3 (0, speed * Time.deltaTime, 0));
+	}
+}
